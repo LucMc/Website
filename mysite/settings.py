9c737be57bb838350429099a6784e54a37f5b398
Luc McCutcheon <lucmccutcheon.home@gmail.com>
2020-09-25 10:49:02 +0100

    initial

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
