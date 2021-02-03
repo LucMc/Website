b7e1273b5c3ad0c7a3984a9182db2b26609fea78
Luc McCutcheon <lucmccutcheon.home@gmail.com>
2021-02-03 22:08:46 +0000

    added img of me

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,3 +147,19 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+       'console': {
+           'class': 'logging.StreamHandler',
+       },
+   },
+   'loggers': {
+       'django': {
+           'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+       },
+   },
+}
